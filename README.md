6eb52d0fa7338e815fd76437118cb46e738e7002
jiangxincode <jiangxinnju@163.com>
2015-05-29 14:23:25 +0800

    init the environment

---
+++ README.md
@@ -0,0 +1,35 @@
+# IdiomSolitaire
+
+## Authors
+
++ JiangXin jiangxinnju@163.com
+
+## Description
+
+A toy for idiom soliteaire. It is vary instering.
+
+If you have some advises, please tell me, open a issue, give me a pull request, or send me a email. 
+
+Hava a good time on it.
+
+## Usage
+
+## Configuration
+
+## Information
+
+## TODO
+
++ add some extra features.
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch(`git check -b my-new-feature`)
+3. Commit your changes(`git commit -am "Add some feature"`)
+4. Push to the branch(`git push origin my-new-feature`)
+5. Create new Pull Request
+
+## License
+
++ The MIT License (MIT) http://mit-license.org/
---
+++ main.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "getopt.h"
+
+#define MAXLENGTH 30
+#define MAXNUM 30000
+#define FILENAME_MAXLENGTH 30
+#define DEFAULT_FILENAME "idiom.txt"
+
+void show_help();
+
+struct list
+{
+    char data[MAXLENGTH];
+    bool is_visited; //标记是否已经遍历
+    struct list *next;
+};
+
+struct list* create_list(char array[][MAXLENGTH],int num);
+void connect(char *ch,struct list* pointer, int num, bool is_loop);
+
+int main(int argc, char* argv[])
+{
+    extern int opterr, optopt;
+    extern char *optarg;
+    char input[MAXLENGTH];
+    char idiom_array[MAXNUM][MAXLENGTH];
+    int count = 0;
+    char c;
+    char filename[FILENAME_MAXLENGTH] = DEFAULT_FILENAME;
+    int show_item_num = 10;
+    bool is_loop  = false;
+    struct list* ptr;
+    FILE *fp;
+    opterr = 0;
+
+    while((c = getopt(argc, argv, "f:n:lh")) != -1)
+    {
+        switch(c)
+        {
+        case 'f':
+            strcpy(filename, optarg);
+            break;
+
+        case 'n':
+            show_item_num = atoi(optarg);
+            break;
+
+        case 'l':
+            is_loop = true;
+            break;
+
+        case 'h':
+            show_help();
+            break;
+
+        case '?':
+            printf("unrecognized option: -%c", optopt);
+            show_help();
+            exit(-1);
+        }
+    }
+
+    fp = fopen(filename, "r");
+
+    for(int i=0; i<MAXNUM; i++)
+    {
+        if(!fgets(idiom_array[i], MAXLENGTH, fp))
+        {
+            break;
+        }
+
+        idiom_array[i][strlen(idiom_array[i])-1] = '\0'; //删除掉换行符
+        count++;
+    }
+
+    count--;
+    ptr=create_list(idiom_array,count);
+    printf("请输入一个成语\n");
+    scanf("%s",input);
+    connect(input,ptr,show_item_num,is_loop);
+    return 0;
+}
+
+struct list* create_list(char array[][MAXLENGTH],int num)
+{
+    struct list *tmp1,*tmp2,*head;
+    head=(struct list *)malloc(sizeof(struct list));
+    strcpy(head->data,array[0]);
+    head->is_visited = false;
+    tmp1=head;
+
+    for(int i=1; i<num; i++)
+    {
+        tmp2=(struct list *)malloc(sizeof(struct list));
+        tmp2->next=NULL;
+        strcpy(tmp2->data,array[i]);
+        tmp2->is_visited = false;
+        tmp1->next=tmp2;
+        tmp1=tmp1->next;
+    }
+
+    return head;
+}
+
+void connect(char *input,struct list* head, int num, bool is_loop)
+{
+    struct list * p=head;
+    int count = 0;
+    char tmp[MAXLENGTH],tmp_head[3],tmp_tail[3];
+    strcpy(tmp,input);
+
+    while(p->next!=NULL)
+    {
+        strncpy(tmp_tail,tmp+6,2);
+        tmp_tail[2]='\0';
+        strncpy(tmp_head,p->data,2);
+        tmp_head[2]='\0';
+
+        if(strcmp(tmp_head,tmp_tail)==0)
+        {
+            if((!is_loop) && (p->is_visited)) //如果不允许循环，且该节点已经遍历过
+            {
+                p=p->next;
+            }
+            else
+            {
+                printf("%s -> ", p->data);
+                p->is_visited = true;
+                strcpy(tmp,p->data);
+                p=head;
+                count++;
+
+                if(count == num)
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            p=p->next;
+        }
+    }
+}
+
+void show_help()
+{
+    fputs("IdiomSolitaire: A toy for idiom solitaire", stdout);
+    fputs("\t-h: Show this help.", stdout);
+    fputs("\t-i filename: Set the dictionary file.", stdout);
+    fputs("\t-l: Be able to loop.", stdout);
+    fputs("\t-n num: Set the idiom item number when output.", stdout);
+    return;
+}
