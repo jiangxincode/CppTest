2cb33cbd30bf3b12d0656f9ee37496cf969fc449
jiangxincode <jiangxinnju@163.com>
2019-08-16 23:49:57 +0800

    main2564.cpp: remove duplicated data

--- single_file_src/main2564.cpp
+++ single_file_src/main2564.cpp
@@ -1,37 +1,29 @@
-﻿/*
-假设一整型数组中的元素按从小到大的顺序排列，编写C语言函数，删除其中的重复元素。
-比如，若一维数组中的数据是：2 2 2 3 4 4 5 6 6 6 6 7 7 8 9 9 10 10 10，
-函数执行后，该一维数组中的数据是：2 3 4 5 6 7 8 9 10。
-*/
+﻿/**
+ * 假设一整型数组中的元素按从小到大的顺序排列，编写C语言函数，删除其中的重复元素。
+ * 比如，若一维数组中的数据是：2 2 2 3 4 4 5 6 6 6 6 7 7 8 9 9 10 10 10，
+ * 函数执行后，该一维数组中的数据是：2 3 4 5 6 7 8 9 10。
+ */
 #include <iostream>
 #include <set>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    int i_array_1[100] = {2,2,2,3,4,4,5,6,6,6,6,7,7,8,9,9,10,10,10};
+int main() {
+    vector<int> in{2, 2, 2, 3, 4, 4, 5, 6, 6, 6, 6, 7, 7, 8, 9, 9, 10, 10, 10};
+    vector<int> out;
     set<int> my_set;
-    int i_array_2[100] = {};
 
-    for(int i=0; i_array_1[i]!=0; i++)
-    {
-        cout << i_array_1[i] << " ";
-        my_set.insert(i_array_1[i]);
+    for (int i : in) {
+        my_set.insert(i);
     }
 
-    cout << endl;
-    int array_end = 0;
-
-    for(int elem:my_set) //需要编译器支持C11标准
-    {
-        i_array_2[array_end] = elem;
-        array_end++;
+    for (int i : my_set) {
+        out.push_back(i);
     }
 
-    for(int i=0; i_array_2[i]!=0; i++)
-    {
-        cout << i_array_2[i] << " ";
+    for (int i : out) {
+        cout << i << " ";
     }
 
     return 0;
