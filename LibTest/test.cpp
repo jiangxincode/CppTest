7edbfb3c14e584ca9c673a2bb45f17c69fbd6d6f
Aloys <jiangxinnju@163.com>
2023-11-12 23:14:36 +0800

    fix compile error for LibTest project

--- LibTest/test.cpp
+++ LibTest/test.cpp
@@ -3,7 +3,7 @@
 //#include "Libjiangxin.h"
 #include "..\\Libjiangxin\\Libjiangxin.h"
 //#pragma comment(lib, "libjiangxin.lib") //指定与静态库一起连接
-#pragma comment(lib, "..\\Debug\\libjiangxin\\libjiangxin.lib") //指定与静态库一起连接
+#pragma comment(lib, "..\\Debug\\libjiangxin.lib") //指定与静态库一起连接
 int main(int argc, char* argv[])
 {
 	printf( "2 + 3 = %d", add( 2, 3 ) );
